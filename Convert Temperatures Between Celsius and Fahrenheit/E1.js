2183f409acc56457fa1ec6e251424be7dfd459ba
rencejke <bonillaclarencejake@gmail.com>
2025-05-10 00:05:46 +0800

    modfied

--- Convert Temperatures Between Celsius and Fahrenheit/E1.js
+++ Convert Temperatures Between Celsius and Fahrenheit/E1.js
@@ -7,8 +7,11 @@ const convertCtoF = (userInputC * (9/5)) + 32
 const convertFtoC =  (userInputF - 32) * (5/9)
 
 
-console.log(`${userInputC}°C is ${convertCtoF}°F`);
-console.log(`${userInputF}°F is ${convertFtoC}°C`);
+console.log(`${userInputC}°C is ${convertCtoF} °F`);
+console.log(`${userInputF}°F is ${convertFtoC} °C`);
 
+//intput value is 60 for celcius and 45 for farenheeit
+//Output
 
-
+//60°C is 140°F
+//45°F is 7.222222222222222°C
